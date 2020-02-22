41afb4a430810d6cfde65b1bf916fdeb6305eeb4
Molly <mollyzhang@ucsc.edu>
2020-02-21 18:05:04 -0800

    created batch from data

--- data_utils.py
+++ data_utils.py
@@ -1,63 +1,80 @@
-from torchtext.data import TabularDataset, Field, RawField, BucketIterator, Iterator
 import torch
 import numpy as np
+import pandas as pd
 import re
 import os
 import pickle
+import itertools
 
 
-def prep_all_data(batch_size=32, path="data", 
-                  device="cuda", bert=True,  
-                  train_file = "train_real.csv",
-                  val_file = "holdout.csv"):
+def prep_all_data(file_dict, batch_size=1, device="cuda"):
     
+    df_train = pd.read_csv(file_dict["train"])
+    df_val = pd.read_csv(file_dict["val_text"], names=["utterances"]).join(
+        pd.read_csv(file_dict["val_label"], names=["IOB Slot tags"]))
+
+    # build vocab from train
     tokenize = lambda x: re.split(" ", x.lower())
-    text_field = Field(sequential=True, tokenize=tokenize, 
-                       lower=True, include_lengths=True)
-    raw_text_field = RawField()
-    label2idx = pickle.load(open("./data/label2idx.pkl", "rb"))
-    label_proc = lambda x: torch.tensor([label2idx[i] for i in x.split(" ")])
-    label_field = RawField(preprocessing=label_proc, is_target=True)
+    vocab = sorted(list(set(list(itertools.chain.from_iterable(    
+    list(df_train["utterances"].apply(tokenize)))))))
+    vocab = ["<pad>", "<unk>"] + vocab
+    text2idx = {text: i for i, text in enumerate(vocab)}
+    idx2text = {i: text for i, text in enumerate(vocab)}
+    
+    # build label from train
+    labels = sorted(list(set(" ".join(df_train["IOB Slot tags"]).split(" "))))
+    labels = ["<UNK>"] + labels
+    np.save("./data/labels.npy", np.array(labels))
+    label2idx = {label: i for i, label in enumerate(labels)}
+    idx2label = {i: label for i, label in enumerate(labels)}
+    
+    # build batch
+    if batch_size != 1:
+        raise(Exception("not implemented"))
 
-    trn, vld = TabularDataset.splits(path=path, 
-        train=train_file, validation=val_file,
-        format='csv', skip_header=True,
-        fields=[("utterances", text_field),
-                #("utterances", raw_text_field), 
-                ("IOB Slot tags", label_field)])
-
-    text_field.build_vocab(trn)
-    train_iter, val_iter = BucketIterator.splits(
-        (trn, vld),
-        batch_sizes=(batch_size, batch_size),
-        device=torch.device(device), 
-        sort_key=lambda x: len(x.utterances), 
-        sort_within_batch=False,
-        repeat=False)
-
-    train_data = BaseWrapper(train_iter, text_field=text_field, path=path, 
-        sample_size=len(trn), batch_size=batch_size)
-    val_data = BaseWrapper(val_iter, text_field=text_field, path=path,
-        sample_size=len(vld), batch_size=batch_size)
-    return train_data, val_data, trn, vld, train_iter, val_iter
+    train_data = Batch(df_train, text2idx, label2idx)
+    val_data = Batch(df_val, text2idx, label2idx)
+    return train_data, val_data
 
 
-
-class BaseWrapper(object):
-    def __init__(self, data, text_field=None, path="data", device="cuda",
-                 sample_size=None, batch_size=None):
-        self.data = data
-        self.batch_size = batch_size
-        self.sample_size = sample_size
-        self.text_field = text_field
-        self.device = device
+class Batch(object):
+    def __init__(self, df, text2idx, label2idx, device="cuda"):
+        self.df = df
+        self.t2i = text2idx
+        self.l2i = label2idx
 
     def __iter__(self):
-        for batch in self.data:
-            x = batch.utterances[0]
-            y = torch.stack(batch.__dict__["IOB Slot tags"], axis=0)
-            yield x, y
-    
-    def __len__(self):
-        return self.sample_size
+        idx = np.random.permutation(self.df.index)    
+        for i in idx:
+            sample = Sample(i, self.df.loc[i], self.t2i, self.l2i)
+            yield sample
+
+
+class Sample(object):
+    def __init__(self, sample_id, data, text2idx, label2idx):
+        self.raw_text = data.utterances
+        self.raw_label = data["IOB Slot tags"]
+        self.t2i = text2idx
+        self.l2i = label2idx
+        self.sample_id = sample_id
+        self.text = torch.tensor(self.convert_text())
+        self.label = torch.tensor(self.convert_label())
+
+    def convert_text(self):
+        self.text = []
+        for t in self.raw_text.lower().split(" "):
+            try:
+                self.text.append(self.t2i[t])
+            except KeyError:
+                self.text.append(self.t2i["<unk>"])
+        return self.text
+
+    def convert_label(self):
+        self.label = []
+        for l in self.raw_label.split(" "):
+            try:
+                self.label.append(self.l2i[l])
+            except KeyError:
+                self.label.append(self.l2i["<UNK>"])
+        return self.label
 
